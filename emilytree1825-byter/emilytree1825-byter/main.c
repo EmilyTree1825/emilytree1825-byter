a61e496b58b352a01875fc11e305bddf90a9ff26
EmilyTree1825 <jjohnson@iMac.local>
2015-06-04 09:21:31 -0400

    Summary #1082931555
    Hello, World!

---
+++ emilytree1825-byter/emilytree1825-byter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  emilytree1825-byter
+//
+//  Created by Judy Johnson on 6/4/15.
+//  Copyright (c) 2015 Judy Johnson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
